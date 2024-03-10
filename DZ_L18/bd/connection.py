8ebb16deaaf48d82e0b380f7b5101a79802df728
zenastyaz <zhartyn.nastya@gmail.com>
2024-03-10 15:12:57 +0200

    update files

--- DZ_L18/bd/connection.py
+++
@@ -1,5 +0,0 @@
-from sqlalchemy import create_engine
-from app.config import Config
-
-
-engine = create_engine(Config.SQLALCHEMY_DATABASE_URI)
--- DZ_L18/bd/init_bd_tables.py
+++ DZ_L18/bd/init_bd_tables.py
@@ -1,28 +1,21 @@
-from sqlalchemy.orm import Session
 from bd.database import db
 from books.model import Book
 from authors.model import Author
-from bd.database import Base
-
-from bd.connection import engine
-
-
-Base.metadata.create_all(engine)
 
 
 def populate_db():
-    with Session(engine) as session:
-        if session.query(Author).count() == 0:
+    with db.session.begin():
+        if db.session.query(Author).count() == 0:
             authors_list = [
                 Author(name='Tolstoy'),
                 Author(name='Dostoevsky'),
                 Author(name='Chekhov'),
                 Author(name='Pushkin')
             ]
-            session.add_all(authors_list)
-            session.commit()
+            db.session.add_all(authors_list)
+            db.session.commit()
 
-        if session.query(Book).count() == 0:
+        if db.session.query(Book).count() == 0:
             books_list = [
                 Book(name='War and Peace', author_id=1),
                 Book(name='Anna Karenina', author_id=1),
@@ -31,8 +24,8 @@ def populate_db():
                 Book(name='The Cherry Orchard', author_id=3),
                 Book(name='Eugene Onegin', author_id=4)
             ]
-            session.add_all(books_list)
-            session.commit()
+            db.session.add_all(books_list)
+            db.session.commit()
 
 
 populate_db()
